3bccf5ffed89bdb834a56a357291e61edfec15f7
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-04-13 20:08:49 +0300

    OK

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch (sortedArr, key){
+    sortedArr.sort((a, b) => a - b)
+    let start = 0;
+    let end = sortedArr.length - 1;
+    return binarySearchRun(sortedArr, key, start, end);
 }
+const binarySearchRun = (sortedArr, key, start, end) =>{
+    let middle = Math.floor((start + end) / 2);
+    if (end < start){
+         return -1;
+    }else if (key === sortedArr[middle]){
+        return middle;
+    }else if (key < sortedArr[middle]){
+        return binarySearchRun(sortedArr, key, start, middle - 1);
+    } else if (key > sortedArr[middle]){
+        return binarySearchRun(sortedArr, key, middle + 1, end);
+    }
+}
+
+
+
+    /*while (start <= end) {                            don't pass 2 tests
+        let middle = Math.floor((start + end) / 2);    
+        if (sortedArray[middle] === key) {
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return -1;
+}*/
 
 module.exports = binarySearch
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,17 @@
 const binarySearch = require('../index')
 
 test('Функция должна возвращать индекс элемента', () => {
-    
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)).toBe(2);
+    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'C')).toBe(2);
 })
 
 test('Функция должна быть определена', () => {
-    
+    expect(binarySearch).toBeDefined();
 })
 
 test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
+    expect(binarySearch([1,  2,  3], 4)).toBe(-1);
+    expect(binarySearch([1,  2, 3, 5, 7], 8)).toBe(-1);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 20)).toBe(-1);
+    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'R')).toBe(-1);
 })
